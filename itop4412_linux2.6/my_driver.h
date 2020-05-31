3fece2e0b802bc10658ad1f18a80fff9a6c37fb4
Saint-Arrow <924698880@qq.com>
2020-05-30 18:27:46 -0700

    tasklet_test

---
+++ itop4412_linux2.6/my_driver.h
@@ -0,0 +1,10 @@
+#ifndef MY_DRIVER
+#define MY_DRIVER
+
+
+#include <linux/sched.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/time.h>
+#endif
